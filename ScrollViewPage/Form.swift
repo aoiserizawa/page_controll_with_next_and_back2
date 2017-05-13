18efb1a619454dddbcfd53da973ddb99824fa51e
Alvin Joseph Valdez <alvin@directworksmedia.com>
2017-05-13 20:44:54 +0800

    making getting the form on each page

---
+++ ScrollViewPage/Form.swift
@@ -0,0 +1,24 @@
+//
+//  Form.swift
+//  ScrollViewPage
+//
+//  Created by Alvin Joseph Valdez on 13/05/2017.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class Form: UIView {
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var form1Textfield: UITextField!
+    @IBOutlet weak var form2TextField: UITextField!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- ScrollViewPage/ViewController.swift
+++ ScrollViewPage/ViewController.swift
@@ -8,9 +8,37 @@
 
 import UIKit
 
+class Page {
+    var model: UIView
+    var nibName: String
+    init(model: UIView, nibName: String) {
+        self.model = model
+        self.nibName = nibName
+    }
+}
+
+class Pages{
+    
+    var model: NSObject
+    var items: [Page]
+    var height: CGFloat
+    var identifier: String
+    
+    init(model: NSObject, items: [Page], height: CGFloat, identifier: String) {
+        self.model = model
+        self.items = items
+        self.height = height
+        self.identifier = identifier
+    }
+}
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var featureScrollView: UIScrollView!
+    
+    
+    var pages: [Page] = [Page]()
     
     let feature1 = ["title": "watch", "price": "$0.99", "image": "1"]
     
@@ -25,32 +53,50 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         featureArray = [feature1, feature2, feature3]
         
+        
+        self.pages.append(Page(model: FeatureView(), nibName: "Feature"))
+        self.pages.append(Page(model: FeatureView(), nibName: "Feature"))
+        
+        
         featureScrollView.isPagingEnabled = true
-        featureScrollView.contentSize = CGSize(width: self.view.bounds.width * CGFloat(featureArray.count), height: 250)
+        featureScrollView.contentSize = CGSize(width: self.view.bounds.width * CGFloat(self.pages.count), height: 250)
+        
         featureScrollView.showsHorizontalScrollIndicator = false
         
         loadFeatures()
-        
         
     }
     
     func loadFeatures() {
         
-        for (index, feature) in featureArray.enumerated(){
+        for (index, feature) in self.pages.enumerated(){
             
-            if let featureView = Bundle.main.loadNibNamed("Feature", owner: self, options: nil)?.first as?FeatureView {
+            print(index)
+            if let featureView = Bundle.main.loadNibNamed(feature.nibName, owner: self, options: nil)?[index] as? Form {
                 
-                featureView.featureImageView.image = UIImage(named: feature["image"]!)
+                featureView.nextButton.tag = index
                 
-                featureView.titleLabel.text = feature["title"]
+//                if index == 0 {
+//                    featureView.backButton.isHidden = true
+//                }
                 
-                featureView.priceLabel.text = feature["price"]
+//                featureView.backButton.tag = index
+                
+                featureView.nextButton.addTarget(self, action: #selector(ViewController.buyFeature(sender:)), for: .touchUpInside)
+                
+//                featureView.backButton.addTarget(self, action: #selector(ViewController.backAction(sender:)), for: .touchUpInside)
+                
+                featureView.frame.size.width = self.view.bounds.size.width
+                featureView.frame.origin.x = CGFloat(index) * self.view.bounds.size.width
+                featureScrollView.addSubview(featureView)
+                
+            }else if let featureView = Bundle.main.loadNibNamed(feature.nibName, owner: self, options: nil)?[index] as? FeatureView {
                 
                 featureView.purchaseButton.tag = index
                 
-                if index == 0 {
-                    featureView.backButton.isHidden = true
-                }
+//                if index == 0 {
+//                    featureView.backButton.isHidden = true
+//                }
                 
                 featureView.backButton.tag = index
                 featureView.purchaseButton.addTarget(self, action: #selector(ViewController.buyFeature(sender:)), for: .touchUpInside)
@@ -68,6 +114,8 @@ class ViewController: UIViewController {
     func buyFeature (sender:UIButton) {
         
         
+        print(sender.tag)
+        
         var frame = featureScrollView.frame
         
         let pageNumber = CGFloat(sender.tag + 1)
@@ -79,6 +127,12 @@ class ViewController: UIViewController {
         frame.origin.y = 0;
         featureScrollView.scrollRectToVisible(frame, animated: true)
         
+        if sender.tag == 0 {
+            var formView = featureScrollView.subviews[0] as! Form
+            
+            print(formView.form1Textfield.text)
+            print(formView.form2TextField.text)
+        }
         
     }
     
